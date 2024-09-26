b016273eeb5cfd9b81cac143258f055ca5d06186
kostia-s920 <kostia.s920@gmail.com>
2024-09-26 20:08:58 +0300

    Домашка

--- lesson_1.py
+++ lesson_1.py
@@ -1,19 +1,1 @@
 print("Hello World!")
-
-
-# коментар
-
-# escape послідовності
-# # \n -> перенесення на новий рядок
-# print("Hello\nworld")
-# # \t -> табуляція -> 4 пробіли. (буває в консолі 2 або 8 пробіли)
-# print("Hello\n\tworld")
-# \ -> дзеркалювання, екранування – якщо необхідно службовий символ зробити друкованим
-# print("Hello\\n\\t\"world\"")
-# print("\\\\\\\\\\")
-#
-#
-# print("Hello world","Hello world","Hello world", sep=" - ")
-# print("Hello world", "print", sep=" - ")
-
-print("test")
