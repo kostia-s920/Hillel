51ba678c4533aabab280c4fa642998583ce5c6c1
kostia-s920 <kostia.s920@gmail.com>
2024-09-30 21:31:30 +0300

    Виконав ДЗ 2

--- lesson_1.py
+++ lesson_1.py
@@ -1,34 +1,34 @@
-# print(2 ** 8)
-# print(2 // 3)
-# print(2 % 3)
+# # print(2 ** 8)
+# # print(2 // 3)
+# # print(2 % 3)
+# #
+# # # 274=> 2 +7 +4
 #
-# # 274=> 2 +7 +4
-
-# number = 111
-# n1 = number // 100
-# n2 = number % 100 // 10
-# n2_v2 = number // 10 % 10
-# n3 = number % 10
+# # number = 111
+# # n1 = number // 100
+# # n2 = number % 100 // 10
+# # n2_v2 = number // 10 % 10
+# # n3 = number % 10
+# #
+# # # print(n1)
+# # # print(n2)
+# # # print(n3)2
 #
-# # print(n1)
-# # print(n2)
-# # print(n3)2
-
+# #
+# # print(n1, n2, n3, sep="\n")
+# #
+# # print(f"{n1}\{n2}\n{n3}")
 #
-# print(n1, n2, n3, sep="\n")
+# #
 #
-# print(f"{n1}\{n2}\n{n3}")
-
+# # first_numer = int(input("enter first number: "))
+# # second_number = int(input(" enter second number: "))
+# # result = first_numer + second_number
+# # print(result)
 #
-
-# first_numer = int(input("enter first number: "))
-# second_number = int(input(" enter second number: "))
-# result = first_numer + second_number
-# print(result)
-
-print("перевірка на конфлік")
-print("перевірка на конфлік другий")
-
-print("Привіт з Першої таски")
-print("А це вже для конфлікту")
-print("А це вже для конфлікту 2")
+# print("перевірка на конфлік")
+# print("перевірка на конфлік другий")
+#
+# print("Привіт з Першої таски")
+# print("А це вже для конфлікту")
+# print("А це вже для конфлікту 2")
